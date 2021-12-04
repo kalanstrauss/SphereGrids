1e2b747d4d48884b6d9e4a07e242344771e4085c
kalanstrauss <kstrau1@saic.edu>
2021-12-03 18:17:10 -0800

    change sketch ty Doug

--- docs/sketches/SphereGridSketch.js
+++ docs/sketches/SphereGridSketch.js
@@ -1,12 +1,22 @@
 let sphereGrid;
 let sphereTexture;
 let canvas;
-let canvasScalar = 0.1
-// let button;
+
+// let button
+
+// DOUG: I created this function to calculate the size the canvas should be based on how large the screen is, with some constraints
+function getCanvasSize() {
+  const UPPER_CANVAS_SIZE = 500; // the largest the canvas could possibly be (change this if you want)
+  const LOWER_CANVAS_SIZE = 80; // the smallest the canvas could possibly be (change this if you want)
+
+  // DOUG: as you stretch or shrink the page, the size of the sphere will grow or shrink between
+  // a screen size of 1112px (I took this number from your media query in your CSS) and 2560px
+  return map(innerWidth, 1112, 2560, LOWER_CANVAS_SIZE, UPPER_CANVAS_SIZE, true);
+}
 
 function preload() {
   sphereGrid = loadModel("sketches/SphereGrid.obj");
-  sphereTexture = loadImage("sketches/SphereGridOverlay2.png");
+  sphereTexture = loadImage("sketches/SphereGridtexture2.png");
   // button = createButton("Store");
   // button.position (innerWidth/2.6, innerHeight/2.75);
   // button.size (150,150);
@@ -14,16 +24,17 @@ function preload() {
 }
 
 function setup() {
-  canvas = createCanvas(innerWidth*canvasScalar, innerWidth*canvasScalar, WEBGL);
-  // canvas.position(0,0)
+  let canvasSize = getCanvasSize(); // DOUG: setting the canvas size using the function above
+  canvas = createCanvas(canvasSize, canvasSize, WEBGL); 
   canvas.parent("#sketch");
-
 }
 
 function windowResized() {
-    resizeCanvas(innerWidth*canvasScalar,innerWidth*canvasScalar);
+    let newSize = getCanvasSize();
+    resizeCanvas(newSize, newSize);
     background(255);
 }
+
 function draw() {
   stroke(0);
   strokeWeight(0);
@@ -31,9 +42,15 @@ function draw() {
   //orbitControl();
   rotateX(mouseY/-100);
   rotateY(mouseX/-100);
-  scale(4);
+
+  // DOUG: I figured out the radius of the sphere is ~22px by setting its scale to 1
+  // and seeing at what point it started getting cut off by the canvas.
+  const SPHERE_DIAMETER = 22.25;
+
+  // DOUG: by dividing the canvas size by the sphere's diameter, we can find a scale value
+  // that will always make the sphere exactly as large as the canvas
+  let newScale = getCanvasSize()/SPHERE_DIAMETER; 
+  scale(newScale); // scale the sphere with the new scale value
   model(sphereGrid);
   texture(sphereTexture);
-
-  
 }
